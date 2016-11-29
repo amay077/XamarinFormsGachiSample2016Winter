a72d6231c9da9e4868fa022445bfb7a6860c5896
amay077 <okuokuoku@gmail.com>
2016-11-29 21:19:14 +0900

    Apply MVVM

---
+++ XamarinFormsGachiSample2016Winter/ViewModels/FirstPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace XamarinFormsGachiSample2016Winter.ViewModels
+{
+    public class FirstPageViewModel : BindableBase
+    {
+        private string _address;
+        public string Address
+        {
+            get { return _address; }
+            set { SetProperty(ref _address, value); }
+        }
+
+        public FirstPageViewModel()
+        {
+            Address = "hoge";
+        }
+    }
+}
