df5abd46c345664039e7ae799e856792a10d7087
amay077 <okuokuoku@gmail.com>
2016-11-30 01:40:06 +0900

    WIP Add Xamarin.Forms.GoogleMaps

---
+++ Droid/DroidApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace XamarinFormsGachiSample2016Winter.Droid
+{
+	[Application]
+	[MetaData("com.google.android.maps.v2.API_KEY",
+		  Value = "dummy_api_key")]
+	public class DroidApp : Application
+	{
+		public DroidApp(IntPtr javaReference, JniHandleOwnership transfer)
+			: base(javaReference, transfer)
+		{
+		}
+	}
+}
