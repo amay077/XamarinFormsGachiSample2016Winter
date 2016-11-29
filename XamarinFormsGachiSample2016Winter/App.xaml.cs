6b6f1895d93252e20cf0cfc1974495aaa1368174
amay077 <okuokuoku@gmail.com>
2016-11-29 19:13:46 +0900

    initial commit

---
+++ XamarinFormsGachiSample2016Winter/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsGachiSample2016Winter
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new XamarinFormsGachiSample2016WinterPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ XamarinFormsGachiSample2016Winter/XamarinFormsGachiSample2016WinterPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormsGachiSample2016Winter
+{
+	public partial class XamarinFormsGachiSample2016WinterPage : ContentPage
+	{
+		public XamarinFormsGachiSample2016WinterPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
